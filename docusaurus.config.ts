0c71c976d3214f1bb7d3e73bcb58cddfcc69bf1d
Philippe Martin <philippe@cryptobay.space>
2024-05-27 09:24:49 +0200

    only warnings on BrokenLinks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   organizationName: "phybyte",
   projectName: "dapp-guides",
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
